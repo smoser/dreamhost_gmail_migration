f1031799f5e6b213d2f58ff1785b9619906da075
Giles Thompson <giles@papaiti.com>
2014-08-15 01:23:03 -0700

    added env variables for easier editing and wrote a readme

---
+++ README.md
@@ -0,0 +1,43 @@
+# DreamHost to Gmail Email Migration
+
+A few simple commands which migrate email from a DreamHost account to Gmail.
+
+## Installation
+
+You'll need to have [imapsync](http://imapsync.lamiral.info/) installed to run these commands.
+
+On OS X with [Homebrew](http://brew.sh/):
+
+```bash
+$ brew install imapsync
+```
+
+Installation on Linux is beyond the scope of this document.
+
+## Usage
+
+Clone this repo to your computer:
+
+```bash
+$ git clone git@github.com:gilest/dreamhost_gmail_migration.git
+```
+
+Change directories into the repo:
+
+```bash
+$ cd dreamhost_gmail_migration
+```
+
+Edit the variables at the top of `mailsync.sh` to match your email accounts.
+
+Edit `dreamhost.txt` and `gmail.txt` to match your password for the corresponding account.
+
+Finally, grant execute permissions to the shell script, and run it:
+
+```bash
+$ chmod +x mailsync.sh && ./mailsync.sh
+```
+
+## Credit
+
+Mark Seagrief – [Moving to Google Apps with imapsync](http://seagrief.co.uk/2010/12/moving-to-google-apps-with-imapsync/)
--- mailsync.sh
+++ mailsync.sh
@@ -1,9 +1,13 @@
-imapsync --host1 mail.dreamhostdomain.com --port1 993 --user1 me@dreamhostdomain.com --passfile1 pass1.txt --authmech1 PLAIN --ssl1 \
-         --host2 imap.gmail.com --user2 me@gmail.com --passfile2 pass2.txt --authmech2 LOGIN --port2 993 --ssl2 \
-         --exclude "Sent|Trash|Drafts|Junk" \
+export DH_DOMAIN='mail.dreamhostdomain.com'
+export DH_USER='me@dreamhostdomain.com'
+export GMAIL_USER='me@gmail.com'
+
+imapsync --host1 $DH_DOMAIN --port1 993 --user1 $DH_USER --passfile1 dreamhost.txt --authmech1 PLAIN --ssl1 \
+         --host2 imap.gmail.com --user2 $GMAIL_USER --passfile2 gmail.txt --authmech2 LOGIN --port2 993 --ssl2 \
+         --exclude 'Sent|Trash|Drafts|Junk' \
     --skipsize
 
-imapsync --host1 mail.dreamhostdomain.com --port1 993 --user1 me@dreamhostdomain.com --passfile1 pass1.txt --authmech1 PLAIN --ssl1 \
-         --host2 imap.gmail.com --user2 me@gmail.com --passfile2 pass2.txt --authmech2 LOGIN --port2 993 --ssl2 \
-  --folder "INBOX.Sent" --prefix2 '[Gmail]/' --regextrans2 's/Sent$/Sent Mail/' \
+imapsync --host1 $DH_DOMAIN --port1 993 --user1 $DH_USER --passfile1 dreamhost.txt --authmech1 PLAIN --ssl1 \
+         --host2 imap.gmail.com --user2 $GMAIL_USER --passfile2 gmail.txt --authmech2 LOGIN --port2 993 --ssl2 \
+  --folder 'INBOX.Sent' --prefix2 '[Gmail]/' --regextrans2 's/Sent$/Sent Mail/' \
   --skipsize
