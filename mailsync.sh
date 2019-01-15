b5775287fe91c5276afe49c101bf8df976fbe602
Scott Moser <smoser@brickies.net>
2019-01-14 22:14:20 -0500

    add my example

--- mailsync.sh
+++ mailsync.sh
@@ -1,13 +1,23 @@
-export DH_DOMAIN='mail.dreamhostdomain.com'
-export DH_USER='me@dreamhostdomain.com'
-export GMAIL_USER='me@gmail.com'
+#!/bin/bash
 
-imapsync --host1 $DH_DOMAIN --port1 993 --user1 $DH_USER --passfile1 dreamhost.txt --authmech1 PLAIN --ssl1 \
-         --host2 imap.gmail.com --user2 $GMAIL_USER --passfile2 gmail.txt --authmech2 LOGIN --port2 993 --ssl2 \
-         --exclude 'Sent|Trash|Drafts|Junk' \
-    --skipsize
+# put password for 'me@dreamhostdomain.com' in 'me@dreamhostdomain.com.pass'
+# put password for gmail in 'gmail.txt'
+user="me@dreamhostdomain.com"
+DH_DOMAIN="mail.foo.com"
+DH_USER="$user"
+GMAIL_USER="example@gmail.com"
 
-imapsync --host1 $DH_DOMAIN --port1 993 --user1 $DH_USER --passfile1 dreamhost.txt --authmech1 PLAIN --ssl1 \
-         --host2 imap.gmail.com --user2 $GMAIL_USER --passfile2 gmail.txt --authmech2 LOGIN --port2 993 --ssl2 \
-  --folder 'INBOX.Sent' --prefix2 '[Gmail]/' --regextrans2 's/Sent$/Sent Mail/' \
+# prefix gets user@foo for user@foo.com
+prefix="${user%.*}/"
+
+common=(
+  "--host1=$DH_DOMAIN" --port1=993 --user1="$DH_USER" --passfile1 "$user.pass"
+  --authmech1=PLAIN --ssl1
+  "--user2=$GMAIL_USER" "--passfile2=gmail.txt" --gmail2
+  --authmech2=LOGIN --port2=993 --ssl2
+  "--prefix2=$prefix"
   --skipsize
+  # --dry # use that to run dry run
+)
+
+imapsync "${common[@]}" --exclude 'Trash|Drafts|Junk'
